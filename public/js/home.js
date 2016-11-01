5fccae23a6c7b6008c096380b65db44d13870667
seanmmcpherson <mcphersm@rose-hulman.edu>
2016-10-31 21:57:54 -0400

    small edits to home
    working on profile and pulling out navbar code to a factory file

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,5 @@
 (function() {
-	data = [{
+	buyData = [{
 		image: './images/book.png',
 		title: 'book title',
 		price: 'price'
@@ -10,11 +10,32 @@
 		price: 'another price'
 	}];
 
-	for(var i=0; i<data.length; i++) {
-		var html = "<div><div><p>"+data[i].title+"</p>";
-		html+="<p>"+data[i].price+"</p></div>";
-		html += "<div><img src=" + data[i].image + "></img></div></div></br>";
+	sellData = [{
+		image: './images/book.png',
+		title: 'book title',
+		price: 'Sell price'
+	},
+	{
+		image: './images/book.png',
+		title: 'another title',
+		price: 'another sell price'
+	}];
+
+	for(var i=0; i<buyData.length; i++) {
+		var html = "<div><div><p>"+buyData[i].title+"</p>";
+		html+="<p>"+buyData[i].price+"</p></div>";
+		html += "<div><img src=" + buyData[i].image + "></img></div></div></br>";
 		var searchdiv = document.getElementById('buy-search-div'); 
+		// console.log(searchdiv);
+		
+		searchdiv.innerHTML += html;
+	}
+
+	for(var i=0; i<sellData.length; i++) {
+		var html = "<div><div><p>"+sellData[i].title+"</p>";
+		html+="<p>"+sellData[i].price+"</p></div>";
+		html += "<div><img src=" + sellData[i].image + "></img></div></div></br>";
+		var searchdiv = document.getElementById('sell-search-div'); 
 		// console.log(searchdiv);
 		
 		searchdiv.innerHTML += html;
