ddd8db3a526ee42761835b0490eb14f9f6d6ee4a
seanmmcpherson <mcphersm@rose-hulman.edu>
2016-11-05 23:31:51 -0400

    home and profile updates
    pulling live data and home looks less like ass (resembles bottom portion
    of profiles). There is a weird issue I've discovered regarding string
    comparison that messes with id verification of books and orders. Revisit
    this later.

--- public/js/home.js
+++ public/js/home.js
@@ -1,44 +1,148 @@
 (function() {
-	buyData = [{
-		image: './images/book.png',
-		title: 'book title',
-		price: 'price'
-	},
-	{
-		image: './images/book.png',
-		title: 'another title',
-		price: 'another price'
-	}];
+	"use strict";
+	var apiUrl = "http://localhost:3000/";
+	var buyOrders, buyOrderBooks, sellOrders, sellOrderBooks;
 
-	sellData = [{
-		image: './images/book.png',
-		title: 'book title',
-		price: 'Sell price'
-	},
-	{
-		image: './images/book.png',
-		title: 'another title',
-		price: 'another sell price'
-	}];
+	function setup() {
 
-	for(var i=0; i<buyData.length; i++) {
-		var html = "<div><div><p>"+buyData[i].title+"</p>";
-		html+="<p>"+buyData[i].price+"</p></div>";
-		html += "<div><img src=" + buyData[i].image + "></img></div></div></br>";
-		var searchdiv = document.getElementById('buy-search-div'); 
-		// console.log(searchdiv);
-		
-		searchdiv.innerHTML += html;
+		getBuyOrders();
+		getSellOrders();
+		setTimeout(function () {populateOrders()}, 260);
 	}
 
-	for(var i=0; i<sellData.length; i++) {
-		var html = "<div><div><p>"+sellData[i].title+"</p>";
-		html+="<p>"+sellData[i].price+"</p></div>";
-		html += "<div><img src=" + sellData[i].image + "></img></div></div></br>";
-		var searchdiv = document.getElementById('sell-search-div'); 
-		// console.log(searchdiv);
-		
-		searchdiv.innerHTML += html;
+	function getBuyOrders() {
+		$.ajax({
+            url: apiUrl + "buyOrders/",
+            type: 'GET',
+            dataType: 'JSON',
+            success: function (data) {
+                if (data) {
+                    buyOrders = data;
+                } else {
+                    console.log("Buy order info could not get got");
+                }
+            },
+            error: function (req, status, err) {
+                console.log(err, status, req);
+            }
+        });
+
+		setTimeout(function() {populateBuyOrderBooks()}, 100);
 	}
+
+	function getSellOrders() {
+		$.ajax({
+            url: apiUrl + "sellOrders/",
+            type: 'GET',
+            dataType: 'JSON',
+            success: function (data) {
+                if (data) {
+                    sellOrders = data;
+                } else {
+                    console.log("Sell order info could not get got");
+                }
+            },
+            error: function (req, status, err) {
+                console.log(err, status, req);
+            }
+        });
+
+        setTimeout(function () {populateSellOrderBooks()}, 100);
+	}
+
+	function populateBuyOrderBooks() {
+		buyOrderBooks = [];
+		for(var i = 0; i < buyOrders.length; i++) {
+			$.ajax({
+	            url: apiUrl + "books/" + buyOrders[i].textbook,
+	            type: 'GET',
+	            dataType: 'JSON',
+	            success: function (data) {
+	                if (data) {
+	                    buyOrderBooks.push(data);
+	                } else {
+	                    console.log("Buy order books could not get got");
+	                }
+	            },
+	            error: function (req, status, err) {
+	                console.log(err, status, req);
+	            }
+        	});
+		}
+	}
+
+	function populateSellOrderBooks() {
+		sellOrderBooks = [];
+		for(var i = 0; i < sellOrders.length; i++) {
+			$.ajax({
+	            url: apiUrl + "books/" + sellOrders[i].textbook,
+	            type: 'GET',
+	            dataType: 'JSON',
+	            success: function (data) {
+	                if (data) {
+	                    sellOrderBooks.push(data);
+	                } else {
+	                    console.log("Sell order books could not get got");
+	                }
+	            },
+	            error: function (req, status, err) {
+	                console.log(err, status, req);
+	            }
+        	});
+		}
+	}
+
+
+	function populateOrders() {
+
+		var buyData = [{
+			image: './images/book.png',
+			title: 'book title',
+			price: 'price'
+		},
+		{
+			image: './images/book.png',
+			title: 'another title',
+			price: 'another price'
+		}];
+
+		var sellData = [{
+			image: './images/book.png',
+			title: 'book title',
+			price: 'Sell price'
+		},
+		{
+			image: './images/book.png',
+			title: 'another title',
+			price: 'another sell price'
+		}];
+
+		for(var i=0; i<buyOrders.length; i++) {
+			var html = "<div><div><p>"+buyOrderBooks[i].title+"</p>";
+			html+="<p>"+buyOrders[i].price+"</p></div>";
+			// html += "<div><img src=" + buyOrderBooks[i].imagePath + "></img></div></div></br>";
+			html += "<div><img src='./images/textbookcover.jpg'></div></div></br>"
+			var searchdiv = document.getElementById('buy-search-div'); 
+			// console.log(searchdiv);
+			
+			searchdiv.innerHTML += html;
+		}
+
+		for(var i=0; i<sellOrders.length; i++) {
+			var html = "<div><div><p>"+sellOrderBooks[i].title+"</p>";
+			html+="<p>"+sellOrders[i].price+"</p></div>";
+			// html += "<div><img src=" + sellData[i].image + "></img></div></div></br>";
+			html += "<div><img src='./images/textbookcover.jpg'></div></div></br>"
+			var searchdiv = document.getElementById('sell-search-div'); 
+			// console.log(searchdiv);
+			
+			searchdiv.innerHTML += html;
+		}
+	}
+
+	$(window).on('load', function () {
+        //load in initial state
+        setup();
+    });
 
 })();
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,92 +1,194 @@
 (function() {
-	profileData = [{
-			image: "./images/user-blank.png",
-			firstName: "John",
-			lastName: "Doe",
-			year: "Senior",
-			major: "Computer Science",
-			bought: 3,
-			sold: 4,
-			rating: 95
-	}];
-
-	selling = [{
-			image: './images/book.png',
-			title: 'title1',
-			price: 'price1'
-		},
-		{
-			image: './images/book.png',
-			title: 'title2',
-			price: 'price2'
-		},
-		{
-			image: './images/book.png',
-			title: 'title3',
-			price: 'price3'
-		},
-		{
-			image: './images/book.png',
-			title: 'title4',
-			price: 'price4'
-	}];
-
-	buying = [{
-			image: './images/book.png',
-			title: 'title3',
-			price: 'price3'
-		},
-		{
-			image: './images/book.png',
-			title: 'title4',
-			price: 'price4'
-	}];
+	"use strict";
+	var apiUrl = "http://localhost:3000/";
+	var books, currUser, buyOrders, buyOrderBooks, sellOrders, sellOrderBooks, currUserID;
 
 	var isSellinghtml = " is selling:</p></div>";
 	var isBuyinghtml = " is looking for:</p></div>";
 
-	for (var i = 0; i < profileData.length; i++){
-		var html = "<div id='img'><img id='profilePic' src=" + profileData[i].image + "></img></div>";
-		html += "<div id='details'><p>" + profileData[i].firstName + " " + profileData[i].lastName + "</p>";
-		html += "<p>" + profileData[i].year + ", " + profileData[i].major + " major</p>";
-		html += "<p>Bought: " + profileData[i].bought + " books</p>";
-		html += "<p>Sold: " + profileData[i].sold + " books</p>";
-		html += "<p>Rating: " + profileData[i].rating + "%</p>";
+	function setup() {
+
+		getCurrentUser();
+		getBuyOrders();
+		getSellOrders();
+		setTimeout(function () {populateOrders()}, 300);
+	}
+
+	function getCurrentUser() {
+		//hard-coded user selection for now
+		$.ajax({
+            url: apiUrl + "users/5817ff5bf083f3263065d756",
+            type: 'GET',
+            dataType: 'JSON',
+            success: function (data) {
+                if (data) {
+                    currUser = data;
+                    currUserID = currUser._id;
+                    // console.log(currUser);
+                } else {
+                    console.log("User info could not get got");
+                }
+            },
+            error: function (req, status, err) {
+                console.log(err, status, req);
+            }
+        })
+	}
+
+	function getBuyOrders() {
+		$.ajax({
+            url: apiUrl + "buyOrders/",
+            type: 'GET',
+            dataType: 'JSON',
+            success: function (data) {
+                if (data) {
+                    buyOrders = data;
+                } else {
+                    console.log("Buy order info could not get got");
+                }
+            },
+            error: function (req, status, err) {
+                console.log(err, status, req);
+            }
+        });
+
+		setTimeout(function() {populateBuyOrderBooks()}, 100);
+	}
+
+	function getSellOrders() {
+		$.ajax({
+            url: apiUrl + "sellOrders/",
+            type: 'GET',
+            dataType: 'JSON',
+            success: function (data) {
+                if (data) {
+                    sellOrders = data;
+                } else {
+                    console.log("Sell order info could not get got");
+                }
+            },
+            error: function (req, status, err) {
+                console.log(err, status, req);
+            }
+        });
+
+        setTimeout(function () {populateSellOrderBooks()}, 100);
+	}
+
+	function populateBuyOrderBooks() {
+		buyOrderBooks = [];
+		for(var i = 0; i < buyOrders.length; i++) {
+			$.ajax({
+	            url: apiUrl + "books/" + buyOrders[i].textbook,
+	            type: 'GET',
+	            dataType: 'JSON',
+	            success: function (data) {
+	                if (data) {
+	                    buyOrderBooks.push(data);
+	                } else {
+	                    console.log("Buy order books could not get got");
+	                }
+	            },
+	            error: function (req, status, err) {
+	                console.log(err, status, req);
+	            }
+        	});
+		}
+	}
+
+	function populateSellOrderBooks() {
+		sellOrderBooks = [];
+		for(var i = 0; i < sellOrders.length; i++) {
+			$.ajax({
+	            url: apiUrl + "books/" + sellOrders[i].textbook,
+	            type: 'GET',
+	            dataType: 'JSON',
+	            success: function (data) {
+	                if (data) {
+	                    sellOrderBooks.push(data);
+	                } else {
+	                    console.log("Sell order books could not get got");
+	                }
+	            },
+	            error: function (req, status, err) {
+	                console.log(err, status, req);
+	            }
+        	});
+		}
+	}
+
+	function populateOrders() {
+		// for (var i = 0; i < profileData.length; i++){
+		var html = "<div id='img'><img id='profilePic' src=" + currUser.profilePicture + "></img></div>";
+		html += "<div id='details'><p>" + currUser.firstName + " " + currUser.lastName + "</p>";
+		html += "<p>" + currUser.year + ", " + currUser.major + " major</p>";
+		html += "<p>Bought: " + currUser.buyHistory.length + " books</p>";
+		html += "<p>Sold: " + currUser.sellHistory.length + " books</p>";
+		html += "<p>Rating: " + currUser.rating + "/5</p>";
 		html += "</div>";
 		// console.log(searchdiv);
-		isSellinghtml = "<div class='header'><p>" + profileData[i].firstName + isSellinghtml;
-		isBuyinghtml = "<div class='header'><p>" + profileData[i].firstName + isBuyinghtml;
+		isSellinghtml = "<div class='header'><p>" + currUser.firstName + isSellinghtml;
+		isBuyinghtml = "<div class='header'><p>" + currUser.firstName + isBuyinghtml;
 		var info = document.getElementById("info");
-		console.log(info);
 		
 		info.innerHTML += html;
+		// }
+
+		var sellingdiv = document.getElementById('selling');
+		sellingdiv.innerHTML += isSellinghtml;
+		for (var i = 0; i < sellOrders.length; i++){
+			// console.log("Seller: " + sellOrders[i].seller);
+			// console.log("CurrUser: " + currUserID);
+			if  (sellOrders[i].seller === currUserID) {
+				for (var j  = 0; j < sellOrderBooks.length; j++) {
+					if(sellOrders[i].textbook === sellOrderBooks[j]._id) {
+
+						//IMPORTANT: When you figure whatever this equality issue is out, fix it in home.js as well. 
+
+						console.log(typeof sellOrders[i].textbook + " " + sellOrders[i].textbook);
+						console.log(typeof sellOrderBooks[i]._id + " " + sellOrderBooks[i]._id);
+						console.log(sellOrders[i].textbook === sellOrderBooks[j]._id); //what the actual fuck, JS. Go home, you're drunk
+						var html = "<div><div><p>"+sellOrderBooks[i].title+"</p>";
+						html+="<p>"+sellOrders[i].price+"</p></div>";
+						// html += "<div><img src=" + sellOrderBooks[i].imagePath + "></img></div></div></br>";
+						html += "<div><img src='./images/textbookcover.jpg'></div></div></br>"
+
+						sellingdiv.innerHTML += html;
+					}
+				}
+				
+			} else {
+				continue;
+			}
+		}
+		var buyingdiv = document.getElementById('buying');
+		buyingdiv.innerHTML += isBuyinghtml;
+		for (var i = 0; i < buyOrders.length; i++){
+			if (buyOrders[i].buyer === currUserID) {
+				var html = "<div><div><p>"+buyOrderBooks[i].title+"</p>";
+				html+="<p>"+buyOrders[i].price+"</p></div>";
+				// html += "<div><img src=" + buyOrderBooks[i].imagePath + "></img></div></div></br>";
+				html += "<div><img src='./images/textbookcover.jpg'></div></div></br>"
+
+
+				buyingdiv.innerHTML += html;
+			} else {
+				continue;
+			}
+		}
+
+
+		var addNewButton = "<button class='newBook' href=''>+ Add New</button>";
+		sellingdiv.innerHTML += addNewButton;
+		buyingdiv.innerHTML += addNewButton;
 	}
 
-	var sellingdiv = document.getElementById('selling');
-	sellingdiv.innerHTML += isSellinghtml;
-	console.log(sellingdiv);
-	for (var i = 0; i < selling.length; i++){
-		var html = "<div><div><p>"+selling[i].title+"</p>";
-		html+="<p>"+selling[i].price+"</p></div>";
-		html += "<div><img src=" + selling[i].image + "></img></div></div></br>";
-
-		sellingdiv.innerHTML += html;
-	}
-	var buyingdiv = document.getElementById('buying');
-	console.log(buyingdiv);
-	buyingdiv.innerHTML += isBuyinghtml;
-	for (var i = 0; i < buying.length; i++){
-		var html = "<div><div><p>"+buying[i].title+"</p>";
-		html+="<p>"+buying[i].price+"</p></div>";
-		html += "<div><img src=" + buying[i].image + "></img></div></div></br>";
-
-		buyingdiv.innerHTML += html;
-	}
 
 
-	var addNewButton = "<button class='newBook' href=''>+ Add New</button>";
-	sellingdiv.innerHTML += addNewButton;
-	buyingdiv.innerHTML += addNewButton;
+	$(window).on('load', function () {
+        //load in initial state
+        setup();
+    });
 
 
 })();
@@ -107,9 +209,12 @@ function myFunction() {
     	binary = reader.result; // binary data (stored as string), unsafe for most actions
     	base64 = btoa(binary); // base64 data, safer but takes up more memory
     	// console.log(binary);
-    	console.log(base64);
+    	// console.log(base64);
 		var imgTag = document.getElementById("profilePic");
 		imgTag.setAttribute("src", "data:image/png;base64," + base64);
 	}, false);
 	reader.readAsBinaryString(img);
 }
+
+
+//5817ff5bf083f3263065d756
